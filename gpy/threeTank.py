cc844c1098931164bd19e0104d127c84e483f69b
alepp <alepp@arigo.de>
2024-05-22 16:54:04 +0200

    test filterpy: IMM + UKF for threetank

--- gpy/threeTank.py
+++ gpy/threeTank.py
@@ -1,6 +1,17 @@
 import numpy as np
 from numpy import sqrt, sign, random
 
+parameter = {
+    'u': 1.2371e-4,
+    'c13': 2.5046e-5,
+    'c32': 2.5046e-5,
+    'c2R': 1.9988e-5,
+    'g': 9.81 ,
+    'A': 0.0154,
+    'sigmaX': 1e-6,
+    'sigmaY': 5e-4,
+}
+
 class ThreeTank():
 
     def __init__(self, param):
@@ -34,3 +45,29 @@ class ThreeTank():
         y2 = x[1] + random.normal(0, 1) * self.sigmaY
         y3 = x[2] + random.normal(0, 1) * self.sigmaY
         return [y1 , y2, y3]
+
+
+def stateTransition(x, dt):
+    for i in range(0, len(x)):
+        if x[i] < 0:
+            x[i] = 0
+
+    dx1 = 1/parameter['A']*(parameter['u']-parameter['c13']*sign(x[0]-x[2])*sqrt(2*parameter['g']*abs(x[0]-x[2])))
+    dx2 = 1/parameter['A']*(parameter['c32']*sign(x[2]-x[1])*sqrt(2*parameter['g']*abs(x[2]-x[1]))-parameter['c2R']*sqrt(2*parameter['g']*abs(x[1])))
+    dx3 = 1/parameter['A']*(parameter['c13']*sign(x[0]-x[2])*sqrt(2*parameter['g']*abs(x[0]-x[2]))-parameter['c32']*sign(x[2]-x[1])*sqrt(2*parameter['g']*abs(x[2]-x[1])))
+
+    dx1_noisy = dx1 + random.normal(0, 1) * parameter['sigmaX']
+    dx2_noisy = dx2 + random.normal(0, 1) * parameter['sigmaX']
+    dx3_noisy = dx3 + random.normal(0, 1) * parameter['sigmaX']
+    
+    return [
+        dx1_noisy * dt + x[0], 
+        dx2_noisy * dt + x[1], 
+        dx3_noisy * dt + x[2]
+    ]
+    
+def observation(x):
+    y1 = x[0] + random.normal(0, 1) * parameter['sigmaY']
+    y2 = x[1] + random.normal(0, 1) * parameter['sigmaY']
+    y3 = x[2] + random.normal(0, 1) * parameter['sigmaY']
+    return [y1 , y2, y3]
