d21a33e4027fcceecf7cdf038cb6001c8001ce40
alepp <alepp@arigo.de>
2024-10-03 20:44:38 +0200

    test framework for torchfilter

--- torchfilter/test_torchfilter.py
+++ torchfilter/test_torchfilter.py
@@ -1,13 +1,11 @@
 import torchfilter
 from torchfilter import types
 import torch
-import torch.nn as nn
-from typing import Tuple, cast, Optional, List
-import gpytorch
+from typing import Tuple
 import numpy as np
 import matplotlib.pyplot as plt
-import fannypack
-from overrides import overrides
+
+import pandas as pd
 
 from threeTank import ThreeTank, parameter as param
 from helper import createTrainingData
@@ -17,22 +15,41 @@ from threeTank_pytorch import ThreeTankDynamicsModel
 from imm_pf import IMMParticleFilter
 from test_filters import _run_filter
 
-dt=0.1
+from GP_BF import GP_SSM_gpy_multiout, GP_SSM_gpy_LVMOGP, GP_SSM_gpytorch_multitask, BatchIndependentMultitaskGPModel
+
+
+simCounter = 11
+
+verbose = True
+MULTI_MODEL = True
+GP = True
+SAVE = False
+
+NORMALIZE = True
+OPTIM_STEPS = 1
+
+# -----------------------------------------------------------------------------
+# model settings
+#------------------------------------------------------------------------------
+
+gp_model = GP_SSM_gpytorch_multitask
+
+
+stateN = 3
+measN = 1
+x0 = np.zeros(stateN) # initial state
+dt = 0.1
 T = 50
-x0 = np.zeros(3)
 
-#param['sigmaX'] = 1e-4
-# param['sigmaY'] = 0
+#PF
+sigma_y = 1e-2
+sigma_x = 1e-7
+S = 100
 
-metaParams = [
-    {'T':T, 'downsample':1},
-    {'T':T, 'downsample':1}, 
-]
-
-metaParams2 = [
-    {'T':50, 'downsample':10}, 
-    {'T':50, 'downsample':10}, 
-]
+#IMMM
+modeN = 2
+mu = [0.9, 0.1] 
+trans = torch.tensor([[0.9, 0.1], [0.1, 0.9]])
 
 param2 = param.copy()
 param2['c13'] = param['c13'] * 4 
@@ -40,17 +57,43 @@ param2['c32'] = param['c32'] * 4
 param2['c2R'] = param['c2R'] * 4
 param2['u'] = 0
 
+
+# -----------------------------------------------------------------------------
+# create training data
+#------------------------------------------------------------------------------
+
+metaParams = [
+    {'T':T, 'downsample':10}, 
+    {'T':T, 'downsample':10}
+]
+
 params = [
-    param,
+    param, 
     param2
 ]
-xD, yD, dxD, tsD = createTrainingData(ThreeTank, params, metaParams, 3, dt, x0, multipleSets=False, plot =False)
 
-xD2, yD2, dxD2, tsD2 = createTrainingData(ThreeTank, [param, param2] , metaParams2, 3, dt, x0, multipleSets=True, plot =False)
+#if GP:
+xD, yD, dxD, tsD = createTrainingData(ThreeTank, params, metaParams, stateN, dt, x0, multipleSets=MULTI_MODEL)
 
-xTest = torch.tensor(xD.transpose()).float()
-yTest = torch.tensor(yD[0:1,:].transpose()).float() #TODO: adjustment needs to be automated
-#uTest = torch.zeros_like(yTest)
+
+# -----------------------------------------------------------------------------
+# create test data
+#------------------------------------------------------------------------------
+
+metaParamT = [
+    {'T':T, 'downsample':1}, 
+    {'T':T, 'downsample':1}
+]
+
+paramT = [
+    param, 
+    param2
+]
+
+xT, yT, dxT, tsT = createTrainingData(ThreeTank, paramT, metaParamT, stateN, dt, x0, multipleSets=False, plot =False)
+
+xTest = torch.tensor(xT.transpose()).float()
+yTest = torch.tensor(yT[0:1,:].transpose()).float() #TODO: adjustment needs to be automated
 uTest = torch.zeros(xTest.shape[0], 1, 1)
 
 testShapeX = list(xTest.shape)
@@ -63,18 +106,98 @@ yTest = yTest.view(*testShapeY)
 testData = (xTest, yTest, uTest)
 
 
+
+#param['sigmaX'] = 1e-4
+# param['sigmaY'] = 0
+
+# metaParams = [
+#     {'T':T, 'downsample':1},
+#     {'T':T, 'downsample':1}, 
+# ]
+
+# metaParams2 = [
+#     {'T':50, 'downsample':10}, 
+#     {'T':50, 'downsample':10}, 
+# ]
+
+# param2 = param.copy()
+# param2['c13'] = param['c13'] * 4 
+# param2['c32'] = param['c32'] * 4
+# param2['c2R'] = param['c2R'] * 4
+# param2['u'] = 0
+
+# params = [
+#     param,
+#     param2
+# ]
+# xD, yD, dxD, tsD = createTrainingData(ThreeTank, params, metaParams, 3, dt, x0, multipleSets=False, plot =False)
+
+# xD2, yD2, dxD2, tsD2 = createTrainingData(ThreeTank, [param, param2] , metaParams2, 3, dt, x0, multipleSets=True, plot =False)
+
+# xTest = torch.tensor(xD.transpose()).float()
+# yTest = torch.tensor(yD[0:1,:].transpose()).float() #TODO: adjustment needs to be automated
+# #uTest = torch.zeros_like(yTest)
+# uTest = torch.zeros(xTest.shape[0], 1, 1)
+
+# testShapeX = list(xTest.shape)
+# testShapeX.insert(1,1)
+# testShapeY = list(yTest.shape)
+# testShapeY.insert(1,1)
+# xTest = xTest.view(*testShapeX)
+# yTest = yTest.view(*testShapeY)
+
+# testData = (xTest, yTest, uTest)
+
+
 # xTrain = torch.tensor(xD2.transpose()).float()
 # dxTrain = torch.tensor(dxD2.transpose()).float()
-xTrain1 = torch.tensor(xD2[0].transpose()).float()
-xTrain2 = torch.tensor(xD2[1].transpose()).float()
-dxTrain1 = torch.tensor(dxD2[0].transpose()).float()
-dxTrain2 = torch.tensor(dxD2[1].transpose()).float()
+# xTrain1 = torch.tensor(xD2[0].transpose()).float()
+# xTrain2 = torch.tensor(xD2[1].transpose()).float()
+# dxTrain1 = torch.tensor(dxD2[0].transpose()).float()
+# dxTrain2 = torch.tensor(dxD2[1].transpose()).float()
 
-sigma_y = 1e-2
-sigma_x = 1e-7
+# sigma_y = 1e-2
+# sigma_x = 1e-7
 # shape = list(tensor.shape)
 # shape.insert(dim, 1)
 # return tensor.view(*shape)
+
+def createFilter(stateN:int, measN:int, modeN: int, dt:float, xD, dxD, sigma_x:float, sigma_y:float, param, mu, trans_p, S:int ):
+# init variables for IMM-GP-UKF
+
+    meas_model:torchfilter.base.ParticleFilterMeasurementModel = IdentityParticleFilterMeasurementModel(stateN, measN, (True, False, False), sigma_y) #TODO: adjustment needs to be automated
+
+    if MULTI_MODEL:
+        models = []
+        if GP:
+            for i in range(modeN):
+                gpModel = GpDynamicsModel(stateN, 1, torch.tensor(xD[i].transpose()).float(), torch.tensor(dxD[i].transpose()).float(), sigma_x, normalize=NORMALIZE)
+                gpModel.optimize(verbose=False, iterations=OPTIM_STEPS)
+                models.append(gpModel)
+        else:
+            for i in range(modeN):
+                model =  ThreeTankDynamicsModel(stateN, dt, param[i])
+                models.append(model)
+
+        imm: torchfilter.base.Filter = IMMParticleFilter(
+            dynamics_models=models,
+            measurement_model= meas_model,
+            mu = mu,
+            Pi = trans_p,
+            state_dim= stateN,
+            num_particles=S,
+        )
+
+        return imm
+
+    else:
+        gpModel = GpDynamicsModel(stateN, 1, torch.tensor(xD.transpose()).float(), torch.tensor(dxD.transpose()).float(), sigma_x, normalize=NORMALIZE)
+        gpModel.optimize(OPTIM_STEPS, verbose)
+        pf = torchfilter.filters.ParticleFilter(
+            dynamics_model=gpModel,
+            measurement_model=meas_model
+        )
+        return pf
 
 def _run_imm_filter(
     filter_model: torchfilter.base.Filter,
@@ -116,66 +239,96 @@ def _run_imm_filter(
     assert estimated_states.shape == (T - 1, N, state_dim)
     return estimated_states, estimated_modes
 
-def test_particle_filter(generated_data):
-    """Smoke test for particle filter."""
+def test_particle_filter(filter, generated_data, multiModel:bool):
+    if multiModel:
+        estimates, modes = _run_imm_filter(filter, generated_data)
+    else:
+        estimates = _run_filter(filter, generated_data)
+        modes = None
 
-    gpModel1 = GpDynamicsModel(3, 1, xTrain1, dxTrain1, sigma_x, normalize=True)
-    gpModel1.optimize(verbose=False, iterations=100)
+    true_states = generated_data[0]
 
-    gpModel2 = GpDynamicsModel(3, 1, xTrain2, dxTrain2, sigma_x, normalize=True)
-    gpModel2.optimize(verbose=False, iterations=1)
+    if modes is not None:
+        modes = modes.squeeze().numpy().transpose()
 
-    # gpModel = GpDynamicsModel(3, 1, xTrain, dxTrain, sigma_x, normalize=True)
-    # gpModel.optimize(verbose=False, iterations=100)
+    plotResults(tsT[:-1], true_states.squeeze().numpy().transpose()[:,:-1], estimates.squeeze().numpy().transpose(), muValues=modes)
 
-    model1 =  ThreeTankDynamicsModel(3, dt, param)
-    model2 =  ThreeTankDynamicsModel(3, dt, param2)
 
-    estimates, modes = _run_imm_filter(
-        IMMParticleFilter(
-            dynamics_models=[gpModel1, gpModel2],
-            #dynamics_models=[model1, model2],
-            measurement_model=IdentityParticleFilterMeasurementModel(3, 1, (True, False, False), sigma_y), #TODO: adjustment needs to be automated
-            mu = [0.5, 0.5],
-            Pi = torch.tensor([[0.9, 0.1], [0.1, 0.9]]),
-            state_dim= 3,
-            num_particles=100,
-            #estimation_method='argmax'
-        ),
-        generated_data,
-    )
+    # x1 = estimates[:,:,0].numpy()
+    # x2 = estimates[:,:,1].numpy()
+    # x3 = estimates[:,:,2].numpy()
+    # plt.figure()
+    # plt.plot(tsD[:-1],x1, label = 'x1')
+    # plt.plot(tsD[:-1],x2, label = 'x2')
+    # plt.plot(tsD[:-1],x3, label = 'x3')
 
-    # estimates = _run_filter(
-    #     torchfilter.filters.ParticleFilter(
-    #         dynamics_model=gpModel,
-    #         measurement_model=IdentityParticleFilterMeasurementModel(3, 1, (True, False, False), sigma_y), #TODO: adjustment needs to be automated
-    #     ),
-    #     generated_data,
-    # )
+    # plt.plot(tsD,yD[0,:], label = 'y1')
+    # plt.plot(tsD,yD[1,:], label = 'y2')
+    # plt.plot(tsD,yD[2,:], label = 'y3')
+    # plt.legend()
 
-    x1 = estimates[:,:,0].numpy()
-    x2 = estimates[:,:,1].numpy()
-    x3 = estimates[:,:,2].numpy()
-    plt.figure()
-    plt.plot(tsD[:-1],x1, label = 'x1')
-    plt.plot(tsD[:-1],x2, label = 'x2')
-    plt.plot(tsD[:-1],x3, label = 'x3')
+    # plt.figure()
+    # plt.plot(tsD[:-1],modes[:,:,0].numpy(), label = 'mode1')
+    # plt.plot(tsD[:-1],modes[:,:,1].numpy(), label = 'mode2')
+    # plt.legend()
 
-    plt.plot(tsD,yD[0,:], label = 'y1')
-    plt.plot(tsD,yD[1,:], label = 'y2')
-    plt.plot(tsD,yD[2,:], label = 'y3')
-    plt.legend()
+    # plt.show()
 
-    plt.figure()
-    plt.plot(tsD[:-1],modes[:,:,0].numpy(), label = 'mode1')
-    plt.plot(tsD[:-1],modes[:,:,1].numpy(), label = 'mode2')
-    plt.legend()
 
+def plotResults(
+        time, 
+        zValues, 
+        xValues, 
+        errorValues = None, 
+        varianceValues = None, 
+        likelihoodValues = None, 
+        muValues = None, 
+        folder = None, 
+        simName = None,
+        lower_bound = None, 
+        upper_bound = None, 
+    ):
+    fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+
+    for i in range(stateN):
+        ax[0,0].plot(time, zValues[i, :], label='$z_' + str(i) + '$')
+        ax[0,0].plot(time, xValues[i, :], '--', label='$x_' + str(i) + '$')
+        if lower_bound is not None and upper_bound is not None:
+            ax[0,0].fill_between(time, lower_bound[i, :], upper_bound[i, :], color='gray', alpha=0.5, label='$95\%$ CI' if i == 0 else "")
+    ax[0,0].legend()
+
+    if errorValues is not None:
+        for i in range(stateN):
+            ax[0,1].plot(time, errorValues[i, :], label='$error_' + str(i) + '$')
+        ax[0,1].legend()
+
+    if likelihoodValues is not None:
+        ax[1,0].plot(time, likelihoodValues[0,:], label='likelihood')
+        ax[1,0].legend()
+
+    if MULTI_MODEL and muValues is not None:
+        #fig2, ax[0,1] = plt.subplots(1, 1, figsize=set_size(textWidth, 0.5,(1,1)))
+        for i in range(modeN):
+            ax[1,1].plot(time, muValues[i,:], label='$mu^' + str(i) + '$')
+        ax[1,1].legend()
+        #fig2.savefig('../gaussianProcess.tex/img/modeEstimation.pdf', format='pdf', bbox_inches='tight')
+
+    # save data
+    if SAVE and folder is not None and simName is not None:
+        df_state = pd.DataFrame(data=xValues.transpose(), columns=['x' + str(i) for i in range(stateN)])
+        df_error = pd.DataFrame(data=errorValues.transpose(), columns=['error' + str(i) for i in range(stateN)])
+        df_variance = pd.DataFrame(data=varianceValues.transpose(), columns=['variance' + str(i) for i in range(stateN)])
+        df_time = pd.DataFrame(data=time, columns=['time'])
+        df_measurement = pd.DataFrame(data=zValues.transpose(), columns=['z' + str(i) for i in range(stateN)])
+        df_mu = pd.DataFrame(data=muValues.transpose(), columns=['mu' + str(i) for i in range(modeN)])
+        df = pd.concat([df_time, df_measurement, df_state, df_variance, df_error, df_mu], axis=1)
+
+        df.to_csv(folder + simName + '_data.csv', index=False)
+
+    fig.show()
     plt.show()
 
 
-
-#data = generated_data()
-
-test_particle_filter(testData)
+model = createFilter(stateN, measN, modeN, dt, xD, dxD, sigma_x, sigma_y, params, mu, trans, S)
+test_particle_filter(model, testData, MULTI_MODEL)
 
